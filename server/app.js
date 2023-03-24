81053f67279acc75b63c30beec99c79267f0189f
waqui27 <wqisensei@gmail.com>
2023-03-24 19:17:11 +0530

    Checking with cors disabled

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectToDB = require("./config/db")
-const cors = require("cors")
+//const cors = require("cors")
 const todoRoute = require("./route/todoRoute")
 const userRoute = require("./route/userRoute")
 var cookieParser = require('cookie-parser')
@@ -12,18 +12,18 @@ app.use(express.urlencoded({
     extended: true,
 }));
 
-const corsOptions = {
-    origin: [
-        'https://todo-plus.vercel.app',
-        'http://localhost:3000'
-    ],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
-    credentials: true,
-    maxAge: 600,
-    exposedHeaders: ['*', 'Authorization', ]
-};
-
-app.use(cors(corsOptions));
+//const corsOptions = {
+//    origin: [
+//        'https://todo-plus.vercel.app',
+//        'http://localhost:3000'
+//    ],
+//    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+//    credentials: true,
+//    maxAge: 600,
+//    exposedHeaders: ['*', 'Authorization', ]
+//};
+//
+//app.use(cors(corsOptions));
 
 app.use(cookieParser())
 
